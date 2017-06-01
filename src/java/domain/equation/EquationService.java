6f706d4df2519262349e1f304249db4646419eb2
Florian Dejonckheere <florian@floriandejonckheere.be>
2017-06-01 21:26:47 +0200

    Add EquationGenerator client

---
+++ src/java/domain/equation/EquationService.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain.equation;
+
+/**
+ *
+ * @author florian
+ */
+public class EquationService {
+
+    private static java.util.List<java.lang.Integer> generateEquation(java.lang.String operation, int difficulty) {
+        client.EquationGenerator_Service service = new client.EquationGenerator_Service();
+        client.EquationGenerator port = service.getEquationGeneratorPort();
+        return port.generateEquation(operation, difficulty);
+    }
+    
+}
